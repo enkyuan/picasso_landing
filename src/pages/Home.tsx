f3bbacc92fbf97acfc450a68812bf0062cb58a60
vk1815918 <viswaretaskotra@gmail.com>
2024-04-14 12:29:13 -0500

    added any card animation

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,6 +5,7 @@ export default function Home() {
     <div>
       <Spline scene="https://prod.spline.design/oqhWLPkdxqBcwFzK/scene.splinecode" />
       <Spline scene="https://prod.spline.design/ZzCNJl-5GzU8-DXv/scene.splinecode" />
+      <Spline scene="https://prod.spline.design/p7nl1-ji-pgSLLZj/scene.splinecode" />
     </div>
   );
 }
