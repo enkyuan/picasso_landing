715f3a26ce5bf65b26f083cc62d61bc96560dcb0
Enkang Yuan <yuan_enkang@outlook.com>
2024-04-14 22:55:38 -0500

    reworked animation

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,10 +3,10 @@ import Spline from "@splinetool/react-spline";
 export default function Home() {
   return (
     <div>
-      <Spline scene="https://prod.spline.design/1xBhZTwZ4gGDIaXF/scene.splinecode" />
+      <Spline scene="https://prod.spline.design/wJcnzi3CIJmsr0go/scene.splinecode" />
       <Spline scene="https://prod.spline.design/ZzCNJl-5GzU8-DXv/scene.splinecode" />
-      <Spline scene="https://prod.spline.design/p7nl1-ji-pgSLLZj/scene.splinecode" />
       <Spline scene="https://prod.spline.design/EV39zPMo5WvUdtFF/scene.splinecode" />
+      <Spline scene="https://prod.spline.design/p7nl1-ji-pgSLLZj/scene.splinecode" />
     </div>
   );
 }
