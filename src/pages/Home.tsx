5daacd1f09be28c65dbb0feec84856fd68a54329
vk1815918 <viswaretaskotra@gmail.com>
2024-04-15 15:04:28 -0500

    formatted merge

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,12 +11,8 @@ export default function Home() {
         scene="https://prod.spline.design/ZzCNJl-5GzU8-DXv/scene.splinecode"
       />
       <Spline scene="https://prod.spline.design/EV39zPMo5WvUdtFF/scene.splinecode" />
-<<<<<<< HEAD
-      <Spline scene="https://prod.spline.design/GMPJVKUFZK8XSWhb/scene.splinecode" />
-=======
       <Spline scene="https://prod.spline.design/W6Jj6gD2Qx9AAqlK/scene.splinecode" />
       <Spline scene="https://prod.spline.design/p7nl1-ji-pgSLLZj/scene.splinecode" />
->>>>>>> 37f4ca1113ef57fca754aff258e964c01fe0cd57
     </div>
   );
 }
