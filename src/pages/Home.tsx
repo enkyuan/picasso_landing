37f4ca1113ef57fca754aff258e964c01fe0cd57
Enkang Yuan <yuan_enkang@outlook.com>
2024-04-15 12:41:45 -0500

    backdrop removed + header text styling

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,17 @@
+import React from "react";
+
 import Spline from "@splinetool/react-spline";
 
 export default function Home() {
   return (
     <div>
       <Spline scene="https://prod.spline.design/wJcnzi3CIJmsr0go/scene.splinecode" />
-      <Spline scene="https://prod.spline.design/ZzCNJl-5GzU8-DXv/scene.splinecode" />
+      <Spline
+        style={{ width: 400, height: 400 }}
+        scene="https://prod.spline.design/ZzCNJl-5GzU8-DXv/scene.splinecode"
+      />
       <Spline scene="https://prod.spline.design/EV39zPMo5WvUdtFF/scene.splinecode" />
+      <Spline scene="https://prod.spline.design/W6Jj6gD2Qx9AAqlK/scene.splinecode" />
       <Spline scene="https://prod.spline.design/p7nl1-ji-pgSLLZj/scene.splinecode" />
     </div>
   );
