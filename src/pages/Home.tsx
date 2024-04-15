9fcc88321885a990ef1eaaac84a1428edaff52a1
vk1815918 <viswaretaskotra@gmail.com>
2024-04-14 22:57:24 -0500

    updated pick a card any card

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div>
       <Spline scene="https://prod.spline.design/1xBhZTwZ4gGDIaXF/scene.splinecode" />
       <Spline scene="https://prod.spline.design/ZzCNJl-5GzU8-DXv/scene.splinecode" />
-      <Spline scene="https://prod.spline.design/p7nl1-ji-pgSLLZj/scene.splinecode" />
+      <Spline scene="https://prod.spline.design/GMPJVKUFZK8XSWhb/scene.splinecode" />
       <Spline scene="https://prod.spline.design/EV39zPMo5WvUdtFF/scene.splinecode" />
     </div>
   );
