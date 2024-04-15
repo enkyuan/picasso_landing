39d0c3d29c18f4ce069588825377eea6b7269a8f
Enkang Yuan <yuan_enkang@outlook.com>
2024-04-14 22:35:53 -0500

    fixed initial animation

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,7 @@ import Spline from "@splinetool/react-spline";
 export default function Home() {
   return (
     <div>
-      <Spline scene="https://prod.spline.design/oqhWLPkdxqBcwFzK/scene.splinecode" />
+      <Spline scene="https://prod.spline.design/1xBhZTwZ4gGDIaXF/scene.splinecode" />
       <Spline scene="https://prod.spline.design/ZzCNJl-5GzU8-DXv/scene.splinecode" />
       <Spline scene="https://prod.spline.design/p7nl1-ji-pgSLLZj/scene.splinecode" />
     </div>
