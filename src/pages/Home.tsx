ea54575abcf27fbda6874283239b63b851cbaba8
vk1815918 <viswaretaskotra@gmail.com>
2024-04-14 22:58:48 -0500

    fixed broken site

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,10 +5,6 @@ export default function Home() {
     <div>
       <Spline scene="https://prod.spline.design/wJcnzi3CIJmsr0go/scene.splinecode" />
       <Spline scene="https://prod.spline.design/ZzCNJl-5GzU8-DXv/scene.splinecode" />
-<<<<<<< HEAD
-      <Spline scene="https://prod.spline.design/GMPJVKUFZK8XSWhb/scene.splinecode" />
-=======
->>>>>>> 715f3a26ce5bf65b26f083cc62d61bc96560dcb0
       <Spline scene="https://prod.spline.design/EV39zPMo5WvUdtFF/scene.splinecode" />
       <Spline scene="https://prod.spline.design/p7nl1-ji-pgSLLZj/scene.splinecode" />
     </div>
