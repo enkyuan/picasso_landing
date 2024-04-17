fd8e0f1a6a56ca96551c69a156af8fa35a5203cc
vk1815918 <viswaretaskotra@gmail.com>
2024-04-17 04:05:24 -0500

    contact page updated

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -3,10 +3,27 @@ const Spline = React.lazy(() => import("@splinetool/react-spline"));
 
 export default function WaitList() {
   return (
-    <div>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Spline scene="https://prod.spline.design/GFAU3fGESbgO-jqZ/scene.splinecode" />
-      </Suspense>
+    <div style={{ position: 'relative' }}>
+      <Spline scene="https://prod.spline.design/GFAU3fGESbgO-jqZ/scene.splinecode" />
+      <div style={{
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100%',
+        background: 'rgba(0, 0, 0, 0.5)',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        color: '#fff',
+        fontSize: '2em'
+      }}>
+        <h1>Contact Us</h1>
+        <a href="mailto:company@company.com" style={{ fontSize: '1.5em', color: '#fff' }}>
+          companyuse@picasso.com
+        </a>
+      </div>
     </div>
   );
 }
