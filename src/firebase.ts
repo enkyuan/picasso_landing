37fa7feb30331d6323203c6e7e9c81bf2045bb01
Enkang Yuan <yuan_enkang@outlook.com>
2024-04-17 20:49:36 -0500

    readded env values

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 import { getFirestore, addDoc, collection } from "firebase/firestore";
@@ -8,7 +8,7 @@ import { getFirestore, addDoc, collection } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_REACT_APP_FIREBASE_API_KEY,
+  apiKey: "AIzaSyCrYJPIbE9zXiFaalMvtQg8-xVIA7s_dro",
   authDomain: "picasso-landing.firebaseapp.com",
   projectId: "picasso-landing",
   storageBucket: "picasso-landing.appspot.com",
@@ -21,7 +21,7 @@ console.log(import.meta.env.VITE_REACT_APP_FIREBASE_API_KEY);
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
 
 const db = getFirestore(app);
 
